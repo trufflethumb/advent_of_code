71b71023808c2c1b2219e74af1809dffff356f6d
Kevin Peng <peng.kevin@me.com>
2024-01-18 14:31:42 +0800

    Add Cube enum and parsing of games

--- 2023/day_02/Tests/Day02Test/Day02Tests.swift
+++ 2023/day_02/Tests/Day02Test/Day02Tests.swift
@@ -7,10 +7,35 @@
 
 import XCTest
 
+enum Cube: String {
+    case red, green, blue
+
+    var limit: Int {
+        switch self {
+        case .red: 12
+        case .green: 13
+        case .blue: 14
+        }
+    }
+}
+
 func determinePossibility(_ input: String) -> (id: Int, isPossible: Bool) {
-    let components = input.components(separatedBy: ":")
+    let redCubes = 12
+    let greenCubes = 13
+    let blueCubes = 14
+    let components = input.components(separatedBy: ": ")
     let id = Int(components[0].components(separatedBy: .whitespaces)[1])!
-    return (id, id != 3)
+    for draw in components[1].components(separatedBy: "; ") {
+        for cubeType in draw.components(separatedBy: ", ") {
+            let statement = cubeType.components(separatedBy: " ")
+            let count = Int(statement[0])!
+            let color = Cube(rawValue: statement[1])!
+            if count > color.limit {
+                return (id, false)
+            }
+        }
+    }
+    return (id, true)
 }
 
 final class Day02Tests: XCTestCase {
@@ -29,6 +54,12 @@ final class Day02Tests: XCTestCase {
     func test_example_line3() {
         let input = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
         XCTAssertEqual(determinePossibility(input).id, 3)
+        XCTAssertFalse(determinePossibility(input).isPossible)
+    }
+    
+    func test_example_line4() {
+        let input = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+        XCTAssertEqual(determinePossibility(input).id, 4)
         XCTAssertFalse(determinePossibility(input).isPossible)
     }
 }
