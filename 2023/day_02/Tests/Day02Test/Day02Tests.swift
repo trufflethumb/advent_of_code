8fb68e0c9606bfe9cab9f82c083294f1f0316c1f
Kevin Peng <peng.kevin@me.com>
2024-01-18 14:12:02 +0800

    Add first test case from example

--- 2023/day_02/Tests/Day02Test/Day02Tests.swift
+++ 2023/day_02/Tests/Day02Test/Day02Tests.swift
@@ -7,5 +7,14 @@
 
 import XCTest
 
+func determinePossibility(_ input: String) -> (id: Int, isPossible: Bool) {
+    return (1, true)
+}
+
 final class Day02Tests: XCTestCase {
+    func test_example_line1() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+        XCTAssertEqual(determinePossibility(input).id, 1)
+        XCTAssertTrue(determinePossibility(input).isPossible)
+    }
 }
