178cc3f28b92d2d3ee13a5ac46562fcd69c2f9ae
Kevin Peng <peng.kevin@me.com>
2024-01-18 14:45:19 +0800

    Add MinimumSetTests for part two

---
+++ 2023/day_02/Tests/Day02Test/MinimumSetTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+
+func minimumSet(_ input: String) -> (red: Int, green: Int, blue: Int) {
+    (4, 2, 6)
+}
+
+final class MinimumSetTests: XCTestCase {
+    func test_examplePartTwo_line1() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+        XCTAssertEqual(minimumSet(input).red, 4)
+        XCTAssertEqual(minimumSet(input).green, 2)
+        XCTAssertEqual(minimumSet(input).blue, 6)
+    }
+}
