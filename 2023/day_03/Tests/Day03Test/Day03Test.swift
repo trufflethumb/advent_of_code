f26fe133c42af770549229f8e3f4d2d5bcee2556
Kevin Peng <peng.kevin@me.com>
2024-01-19 21:21:23 +0800

    Day 03/2025 example line 1 complete

--- 2023/day_03/Tests/Day03Test/Day03Test.swift
+++ 2023/day_03/Tests/Day03Test/Day03Test.swift
@@ -8,5 +8,13 @@
 import Day03
 import XCTest
 
+func findParts(_ input: String) -> [Int] {
+    []
+}
+
 final class Day03Tests: XCTestCase {
+    func test_example_line1() {
+        let input = "467..114.."
+        XCTAssertEqual(findParts(input), [])
+    }
 }
