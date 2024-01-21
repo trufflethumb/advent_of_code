6babe01be8233c6d34c6634c968373c0ac7dafb0
Kevin Peng <peng.kevin@me.com>
2024-01-21 13:04:15 +0800

    Add run code for part two

--- 2023/day_03/Sources/Run/main.swift
+++ 2023/day_03/Sources/Run/main.swift
@@ -8,15 +8,30 @@ guard let input = Bundle.module.url(forResource: "input", withExtension: "txt") 
 let content = try String(contentsOf: input)
 
 // Part 1
-let wrongAnswers = [424683: "too low"]
-let result = findParts(content).reduce(0, +)
-let answer = 540212
-if let found = wrongAnswers[result] {
-    print("Got one of the wrong results: \(result) is \(found)")
-} else if answer == result {
-    print("You got it right: \(answer)")
-} else {
-    print("New wrong results: \(result)")
+func partOne() {
+    let wrongAnswers = [424683: "too low"]
+    let result = findParts(content).reduce(0, +)
+    let answer = 540212
+    if let found = wrongAnswers[result] {
+        print("Got one of the wrong results: \(result) is \(found)")
+    } else if answer == result {
+        print("You got it right: \(answer)")
+    } else {
+        print("New wrong results: \(result)")
+    }
 }
+partOne()
 
 // Part 2
+func partTwo() {
+    let wrongAnswers = [Int: String]()
+    let result = findParts(content).reduce(0, +)
+    let answer: Int? = nil
+    if let found = wrongAnswers[result] {
+        print("Got one of the wrong results: \(result) is \(found)")
+    } else if let answer, answer == result {
+        print("You got it right: \(answer)")
+    } else {
+        print("New wrong results: \(result)")
+    }
+}
