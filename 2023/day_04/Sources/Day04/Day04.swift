cde1fd644b284f14c53c8fb40b20e6017b30a6dd
Kevin Peng <peng.kevin@me.com>
2024-01-21 23:34:16 +0800

    Move production code

--- 2023/day_04/Sources/Day04/Day04.swift
+++ 2023/day_04/Sources/Day04/Day04.swift
@@ -1,1 +1,45 @@
 import Foundation
+
+public func parse(_ input: String) -> ([Int], [Int]) {
+    guard let startIndex = input.firstIndex(of: ":") else {
+        fatalError("Invalid input")
+    }
+    let winningNumberStart = input.index(after: startIndex)
+    guard let numbersStart = input.firstIndex(of: "|") else {
+        fatalError("Invalid input")
+    }
+    let winningNumbersList = String(input[winningNumberStart ..< numbersStart])
+
+    let candidateNumberStart = input.index(after: numbersStart)
+    let candidateNumbersList = String(input[candidateNumberStart...])
+
+    let winningNumbers = winningNumbersList
+        .components(separatedBy: .whitespaces)
+        .compactMap(Int.init)
+
+    let candidateNumbers = candidateNumbersList
+        .components(separatedBy: .whitespaces)
+        .compactMap(Int.init)
+
+    return (winningNumbers, candidateNumbers)
+}
+
+public func points(_ winningNumbers: [Int], _ candidates: [Int]) -> Int {
+    let numberOfWins = Set(winningNumbers)
+        .intersection(Set(candidates))
+        .count
+
+    return 2.toThePower(of: numberOfWins - 1)
+}
+
+public extension Int {
+    func toThePower(of exp: Int) -> Int {
+        if exp < 0 { return self }
+        if exp == 0 { return 1 }
+        var multiple = 1
+        for _ in 0 ..< exp {
+            multiple *= self
+        }
+        return multiple
+    }
+}
--- 2023/day_04/Tests/Day04Tests/Day04PartOneTest.swift
+++ 2023/day_04/Tests/Day04Tests/Day04PartOneTest.swift
@@ -1,50 +1,6 @@
 import Day04
 import XCTest
 
-func parse(_ input: String) -> ([Int], [Int]) {
-    guard let startIndex = input.firstIndex(of: ":") else {
-        fatalError("Invalid input")
-    }
-    let winningNumberStart = input.index(after: startIndex)
-    guard let numbersStart = input.firstIndex(of: "|") else {
-        fatalError("Invalid input")
-    }
-    let winningNumbersList = String(input[winningNumberStart ..< numbersStart])
-
-    let candidateNumberStart = input.index(after: numbersStart)
-    let candidateNumbersList = String(input[candidateNumberStart...])
-
-    let winningNumbers = winningNumbersList
-        .components(separatedBy: .whitespaces)
-        .compactMap(Int.init)
-
-    let candidateNumbers = candidateNumbersList
-        .components(separatedBy: .whitespaces)
-        .compactMap(Int.init)
-
-    return (winningNumbers, candidateNumbers)
-}
-
-func points(_ winningNumbers: [Int], _ candidates: [Int]) -> Int {
-    let numberOfWins = Set(winningNumbers)
-        .intersection(Set(candidates))
-        .count
-
-    return 2.toThePower(of: numberOfWins - 1)
-}
-
-extension Int {
-    func toThePower(of exp: Int) -> Int {
-        if exp < 0 { return self }
-        if exp == 0 { return 1 }
-        var multiple = 1
-        for _ in 0 ..< exp {
-            multiple *= self
-        }
-        return multiple
-    }
-}
-
 final class Day04PartOneTest: XCTestCase {
     func test_examplePartOne_line1() {
         let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
