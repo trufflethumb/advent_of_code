0e68a956cc195c494c6434ba106694632afe201b
Kevin Peng <peng.kevin@me.com>
2024-01-21 23:33:19 +0800

    Points calculation

--- 2023/day_04/Tests/Day04Tests/Day04PartOneTest.swift
+++ 2023/day_04/Tests/Day04Tests/Day04PartOneTest.swift
@@ -25,12 +25,33 @@ func parse(_ input: String) -> ([Int], [Int]) {
     return (winningNumbers, candidateNumbers)
 }
 
+func points(_ winningNumbers: [Int], _ candidates: [Int]) -> Int {
+    let numberOfWins = Set(winningNumbers)
+        .intersection(Set(candidates))
+        .count
+
+    return 2.toThePower(of: numberOfWins - 1)
+}
+
+extension Int {
+    func toThePower(of exp: Int) -> Int {
+        if exp < 0 { return self }
+        if exp == 0 { return 1 }
+        var multiple = 1
+        for _ in 0 ..< exp {
+            multiple *= self
+        }
+        return multiple
+    }
+}
+
 final class Day04PartOneTest: XCTestCase {
     func test_examplePartOne_line1() {
         let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
         let (winningNumbers, numbers) = parse(input)
         XCTAssertEqual(winningNumbers, [41, 48, 83, 86, 17])
         XCTAssertEqual(numbers, [83, 86, 6, 31, 17, 9, 48, 53])
+        XCTAssertEqual(points(winningNumbers, numbers), 8)
     }
 
     func test_examplePartOne_line2() {
@@ -38,6 +59,7 @@ final class Day04PartOneTest: XCTestCase {
         let (winningNumbers, numbers) = parse(input)
         XCTAssertEqual(winningNumbers, [13, 32, 20, 16, 61])
         XCTAssertEqual(numbers, [61, 30, 68, 82, 17, 32, 24, 19])
+        XCTAssertEqual(points(winningNumbers, numbers), 2)
     }
 
     func test_examplePartOne_line3() {
@@ -45,5 +67,6 @@ final class Day04PartOneTest: XCTestCase {
         let (winningNumbers, numbers) = parse(input)
         XCTAssertEqual(winningNumbers, [1, 21, 53, 59, 44])
         XCTAssertEqual(numbers, [69, 82, 63, 72, 16, 21, 14, 1])
+        XCTAssertEqual(points(winningNumbers, numbers), 2)
     }
 }
