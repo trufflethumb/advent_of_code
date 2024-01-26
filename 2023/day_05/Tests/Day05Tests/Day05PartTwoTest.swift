0e2c4dfbd4f7de21b935a04b9824ba4134ccf9c0
Kevin Peng <peng.kevin@me.com>
2024-01-26 13:35:30 +0800

    Brute force for part two

--- 2023/day_05/Tests/Day05Tests/Day05PartTwoTest.swift
+++ 2023/day_05/Tests/Day05Tests/Day05PartTwoTest.swift
@@ -1,8 +1,27 @@
 import Day05
 import XCTest
 
+func minLocation(rangesOfSeedInput input: Input) -> Int {
+    var seeds = [Int]()
+    for i in stride(from: 0, to: input.seeds.count, by: 2) {
+        seeds.append(
+            contentsOf:
+                Array(input.seeds[i]..<input.seeds[i] + input.seeds[i + 1])
+        )
+    }
+    return seeds.map { seed in
+        print("seed: \(seed)")
+        let n = numbers(fromSeed: seed, using: input.maps)
+        print(n)
+        return n.last!
+    }
+    .min()!
+}
+
 final class Day05PartTwoTest: XCTestCase {
     func test_examplePartTwo_line1() {
-        XCTAssertEqual(1, 1)
+        let input = parse(input)
+        let sut = minLocation(rangesOfSeedInput: input)
+        XCTAssertEqual(sut, 46)
     }
 }
