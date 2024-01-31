8417d626a1b63a9ee2aea332200fc55651e7439a
Kevin Peng <peng.kevin@me.com>
2024-01-31 21:33:22 +0800

    test_examplePartOne_race1

--- 2023/day_06/Tests/Day06Tests/Day06PartOneTest.swift
+++ 2023/day_06/Tests/Day06Tests/Day06PartOneTest.swift
@@ -2,7 +2,23 @@ import Day06
 import XCTest
 
 final class Day06PartOneTest: XCTestCase {
-    func test_examplePartOne_line1() {
-        XCTAssertEqual(1, 1)
+    let input = """
+    Time:      7  15   30
+    Distance:  9  40  200
+    """
+
+    struct Input {
+        let times: [Int]
+        let distances: [Int]
+    }
+
+    func parse(_ input: String) -> Input {
+        return Input(times: [7], distances: [9])
+    }
+
+    func test_examplePartOne_race1() {
+        let sut = parse(input)
+        XCTAssertEqual(sut.times[0], 7)
+        XCTAssertEqual(sut.distances[0], 9)
     }
 }
