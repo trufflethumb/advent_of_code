0e525af2b811b675ab955a7793ac74d4892fad6b
Kevin Peng <peng.kevin@me.com>
2024-01-31 21:36:05 +0800

    Parse all

--- 2023/day_06/Tests/Day06Tests/Day06PartOneTest.swift
+++ 2023/day_06/Tests/Day06Tests/Day06PartOneTest.swift
@@ -13,12 +13,17 @@ final class Day06PartOneTest: XCTestCase {
     }
 
     func parse(_ input: String) -> Input {
-        return Input(times: [7], distances: [9])
+        let components = input.components(separatedBy: .newlines)
+        let times = components[0].components(separatedBy: .whitespaces).compactMap(Int.init)
+        let distances = components[1].components(separatedBy: .whitespaces).compactMap(Int.init)
+        return Input(times: times, distances: distances)
     }
 
     func test_examplePartOne_race1() {
         let sut = parse(input)
         XCTAssertEqual(sut.times[0], 7)
         XCTAssertEqual(sut.distances[0], 9)
+        XCTAssertEqual(sut.times[1], 15)
+        XCTAssertEqual(sut.distances[1], 40)
     }
 }
