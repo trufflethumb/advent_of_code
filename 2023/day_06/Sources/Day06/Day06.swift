09ceff966f758f10524857e5eb662bd5c052709e
Kevin Peng <peng.kevin@me.com>
2024-02-01 15:49:48 +0800

    Add comments

--- 2023/day_06/Sources/Day06/Day06.swift
+++ 2023/day_06/Sources/Day06/Day06.swift
@@ -23,8 +23,8 @@ public func accelerationDuration(timeLimit: Int, record: Int) -> Int {
     // prevent an infinite loop
     var iterations = 0
 
-    // a value of 63 is chosen as the iteration limit because the upper limit of Int is 2^63 - 1, and the binary search algo is O(log(N)) where log is base 2
-    while iterations < 63 {
+    // A value of 62 is chosen as the iteration limit because the upper limit of Int is 2^63 - 1, and the binary search algo is O(log(N)) where log is base 2. Removing one iteration from 63 because we'll maximally look at half of Int.max.
+    while iterations < 62 {
         let speed = 1 * accDuration // v = a * t
         let timeRemaining = timeLimit - accDuration
         let distance = timeRemaining * speed
@@ -34,6 +34,17 @@ public func accelerationDuration(timeLimit: Int, record: Int) -> Int {
         jumpSize = max(1, jumpSize)
         
         if previousJumpSize == jumpSize {
+            // Adding one will cover the case where
+            // the accelerationDuration is exactly
+            // equal to the distance.
+
+            // When the accelerationDuration does not equal to the distance, we will always arrive at the
+            // number immediately left of the answer
+            // because we would go left
+            // when we see the duration being
+            // larger than the record, followed by
+            // one or more movements to the right,
+            // and stop just before the answer.
             accDuration += 1
             break
         } else if distance > record {
