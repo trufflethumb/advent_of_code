e4355f23d89829555644247a3e34d685787baed9
Kevin Peng <peng.kevin@me.com>
2024-02-03 21:55:17 +0800

    Parsing part one

--- 2023/day_10/Tests/Day10Tests/Day10PartOneTest.swift
+++ 2023/day_10/Tests/Day10Tests/Day10PartOneTest.swift
@@ -2,16 +2,42 @@ import Day10
 import XCTest
 
 struct PartOneInput {
-    let name: String
+    let startingPosition: [Int]
+    let map: [[Character]]
 }
 
 func parsePartOne(_ input: String) -> PartOneInput {
-    .init(name: "1")
+    let components = input.components(separatedBy: .newlines)
+    var startingPosition = [Int]()
+    var array = [[Character]]()
+    var lineArray = [Character]()
+    for (x, line) in components.enumerated() {
+        for (y, character) in line.enumerated() {
+            if startingPosition.isEmpty, character == "S" {
+                startingPosition = [x, y]
+            }
+            lineArray.append(character)
+        }
+        array.append(lineArray)
+        lineArray.removeAll(keepingCapacity: true)
+    }
+    return PartOneInput(startingPosition: startingPosition, map: array)
 }
 
 final class Day10PartOneTest: XCTestCase {
-    func test_examplePartOne_line1() {
+    func test_examplePartOne_startingPosition1() {
         let sut = parsePartOne(example)
-        XCTAssertEqual(sut.name, "1")
+        XCTAssertEqual(sut.startingPosition, [1, 1])
+    }
+
+    func test_examplePartOne_startingPosition2() {
+        let sut = parsePartOne(example2)
+        XCTAssertEqual(sut.startingPosition, [1, 3])
+    }
+
+    func test_examplePartOne_2DArray() {
+        let sut = parsePartOne(example)
+        XCTAssertEqual(String(sut.map[0]), ".....")
+        XCTAssertEqual(String(sut.map[1]), ".S-7.")
     }
 }
